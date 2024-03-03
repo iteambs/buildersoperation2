d26d51fbda09b5e756133aba91fb334dc9c5fbb3
“macqy <magarcia@joyride.com.ph>
2024-03-03 17:10:10 +0800

    Update with Builders Operation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { useLocation } from "react-router-dom";
 import { disablePageScroll, enablePageScroll } from "scroll-lock";
 
+import { buildersOperation } from "../assets";
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
 import Button from "./Button";
@@ -37,7 +38,7 @@ const Header = () => {
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
-          <img src={brainwave} width={190} height={40} alt="Brainwave" />
+          <img src={buildersOperation} width={190} height={40} alt="Brainwave" />
         </a>
 
         <nav
